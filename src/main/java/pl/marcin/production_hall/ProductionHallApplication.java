c43d65671e8cd39c5ede1575f0be29a05da535d3
Marcineq521 <marcinbaran789@gmail.com>
2026-02-23 17:59:25 +0100

    Initial commit

---
+++ src/main/java/pl/marcin/production_hall/ProductionHallApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.production_hall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionHallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionHallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcin/production_hall/ProductionHallApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.production_hall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionHallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
